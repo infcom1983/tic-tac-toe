438d7c312a0fd65e2dc9a82e73e743d64dd8fef0
infcom1983 <infcom@yandex.ru>
2024-12-04 10:35:37 +0700

    Convert CellNumberConverter class to interface

--- src/academy/devonline/tictactoe/Launcher.java
+++ src/academy/devonline/tictactoe/Launcher.java
@@ -17,6 +17,7 @@
 package academy.devonline.tictactoe;
 
 import academy.devonline.tictactoe.component.*;
+import academy.devonline.tictactoe.component.keypad.DesktopNumericKeypadCellNumberConverter;
 
 /**
  * @author devonline
@@ -25,7 +26,7 @@ import academy.devonline.tictactoe.component.*;
 public final class Launcher {
 
     public static void main(final String[] args) {
-        final CellNumberConverter cellNumberConverter = new CellNumberConverter();
+        final CellNumberConverter cellNumberConverter = new DesktopNumericKeypadCellNumberConverter();
         final Game game = new Game(
                 new DataPrinter(cellNumberConverter),
                 new ComputerMove(),
